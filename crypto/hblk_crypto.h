640f34285e068ffcadab346f67b774bb4ccad6f4
ChristianCampos-55 <gamochris@gmail.com>
2022-03-17 19:44:01 -0500

    [FIX]General betty corrections

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,3 +1,6 @@
+#ifndef HBLK_CRYPTO_H
+#define HBLK_CRYPTO_H
+
 # include <openssl/sha.h>
 # include <openssl/ec.h>
 # include <stdint.h>
@@ -8,23 +11,32 @@
  #include <openssl/ecdsa.h>
 
 # define EC_CURVE   NID_secp256k1
-# define PATH_MAX        4096 
+# define PATH_MAX        4096
 # define EC_PUB_LEN 65
 # define SIG_MAX_LEN    72
 # define PRI_FILENAME   "key.pem"
 # define PUB_FILENAME   "key_pub.pem"
 
+/**
+ * struct sig_s - EC Signature structure
+ * @sig: buffer
+ * @len: size
+ */
+
 typedef struct sig_s
 {
-    uint8_t     sig[SIG_MAX_LEN];
-    uint8_t     len;
+	uint8_t     sig[SIG_MAX_LEN];
+	uint8_t     len;
 } sig_t;
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest
+				[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig);
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+					sig_t *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+				sig_t const *sig);
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include "hblk_crypto.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest
+				[SHA256_DIGEST_LENGTH]) {
 
-	if(digest == NULL){
-		return NULL;
+	if (digest == NULL)
+	{
+		return (NULL);
 	}
-	else{
-		return (uint8_t*)SHA256((unsigned char*)s,len,(unsigned char*)digest);
+	else
+	{
+		return ((uint8_t *)SHA256((unsigned char *)s, len, (unsigned char *)digest));
 	}
 }
